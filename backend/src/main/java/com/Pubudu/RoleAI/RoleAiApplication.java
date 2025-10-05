fff68252105b33ddeedc4d19ef1299907725b1fb
pubudubandara <pubudumb@gmail.com>
2025-10-05 22:32:06 +0530

    first commit

---
+++ backend/src/main/java/com/Pubudu/RoleAI/RoleAiApplication.java
@@ -0,0 +1,13 @@
+package com.Pubudu.RoleAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoleAiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/Pubudu/RoleAI/RoleAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pubudu.RoleAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

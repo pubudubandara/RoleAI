f87532d58d8b3d69a8456829e97ee63ad01864b1
pubudubandara <pubudumb@gmail.com>
2025-10-08 23:54:41 +0530

    Update ChatBox height to use full screen height

--- frontend/src/components/ChatBox.tsx
+++ frontend/src/components/ChatBox.tsx
@@ -51,7 +51,7 @@ const ChatBox: React.FC<ChatBoxProps> = ({ selectedRole, selectedModel }) => {
   };
 
   return (
-    <div className="flex flex-col h-full">
+    <div className="flex flex-col h-screen">
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
         {messages.length === 0 ? (
           <div className="text-center text-gray-400 mt-8">

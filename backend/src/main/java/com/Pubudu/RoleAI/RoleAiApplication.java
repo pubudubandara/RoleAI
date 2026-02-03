aeba8a09298f2939d10967c60ffe191a592eac19
pubudubandara <pubudumb@gmail.com>
2026-02-03 19:41:22 +0530

    Add application properties and dotenv configuration; remove sample properties

--- backend/src/main/java/com/Pubudu/RoleAI/RoleAiApplication.java
+++ backend/src/main/java/com/Pubudu/RoleAI/RoleAiApplication.java
@@ -1,5 +1,6 @@
 package com.Pubudu.RoleAI;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RoleAiApplication {
 
 	public static void main(String[] args) {
+		// Load environment variables from .env file
+		Dotenv.configure()
+			.directory("./backend")
+			.ignoreIfMissing()
+			.systemProperties()
+			.load();
+		
 		SpringApplication.run(RoleAiApplication.class, args);
 	}
 
